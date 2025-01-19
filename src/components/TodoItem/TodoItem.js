769695a865483598e5c64b4b4fc236b2e7946714
Pablo de los Santos <estudiodedalo@gmail.com>
2025-01-19 17:58:49 -0300

    props estilos

--- src/components/TodoItem/TodoItem.js
+++ src/components/TodoItem/TodoItem.js
@@ -1,11 +1,21 @@
-import "./TodoItem.css"
+import "./TodoItem.css";
 
 function TodoItem(props) {
   return (
     <li className="TodoItem">
-      <span className="Icon Icon-check Icon-check--active"><i class="fa-solid fa-check"></i></span>
-      <p className="TodoItem-p TodoItem-p--complete">{props.text}</p>
-      <span className="Icon Icon-delete"><i class="fa-solid fa-circle-xmark"></i></span>
+      <span
+        className={`Icon Icon-check ${
+          props.completed && "Icon-check--active"
+        } `}
+      >
+        <i class="fa-solid fa-check" />
+      </span>
+      <p className={`TodoItem-p ${props.completed && "TodoItem-p--complete"}`}>
+        {props.text}
+      </p>
+      <span className="Icon Icon-delete">
+        <i class="fa-solid fa-circle-xmark" />
+      </span>
     </li>
   );
 }
